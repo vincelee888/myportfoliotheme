7ed36ef46e6dec1fa24ef5b5775947037ba54cd3
Ben <benjaminjohnmarley@gmail.com>
2017-05-25 10:24:36 +0100

    Css minified

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,24 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./styles/'));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+
+
+
+
+gulp.task('minify-css', function() {
+  return gulp.src('./styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./'));
 });

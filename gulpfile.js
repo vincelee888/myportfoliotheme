6a9a42a0345dea1cbfaeb6fea780cbe9f5c72376
Ben Marley <benjaminjohnmarley@gmail.com>
2017-07-04 20:23:18 +0100

    testing

--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,6 @@ gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sassify']);
 });
 
-
-
-
-
 gulp.task('minify-css', function() {
   return gulp.src('./styles/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
